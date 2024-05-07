d0634bec0b4c766fef2adda77bee701a2657e447
Matthew Zenn <spyguy57@gmail.com>
2024-05-07 08:56:31 -0400

    Added c#

--- Oats/Quake3.cs
+++ Oats/Quake3.cs
@@ -1,18 +1,15 @@
-static void Main( string[] args )
-{
-    Console.WriteLine( $"{Q_rsqrt( 2.43f )}" );
-}
-
-static float Q_rsqrt( float number )
+static float Q_rsqrt(float number)
 {
     float x2 = number * 0.5F;
     float y = number;
-    int i = BitConverter.SingleToInt32Bits( y );
+    int i = BitConverter.SingleToInt32Bits(y);
 
-    i = 0x5f3759df - ( i >> 1 );
-    y = BitConverter.Int32BitsToSingle( i );
+    i = 0x5f3759df - (i >> 1);
+    y = BitConverter.Int32BitsToSingle(i);
 
-    y = y * ( 1.5F - ( x2 * y * y ) );
+    y = y * (1.5F - (x2 * y * y));
 
     return y;
 }
+
+Console.WriteLine($"{Q_rsqrt(2.43f)}");
