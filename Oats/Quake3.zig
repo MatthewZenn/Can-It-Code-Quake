7447b6c2c0a06b8d31c7424060a46cad632f3fc5
Matthew Zenn <spyguy57@gmail.com>
2024-05-06 21:38:59 -0400

    added Zig

---
+++ Oats/Quake3.zig
@@ -0,0 +1,11 @@
+const std = @import("std");
+
+var number: f32 = 0.15625;
+
+var x: i64 = 0;
+var y: f32 = 0.0;
+var x2: i32 = number;
+
+pub fn main() void {
+    std.debug.print("Hello world", .{});
+}
