5d8d3b0f597f810fbb3bc19125eff35cc62722d7
Mladen Macanovic <mladen.macanovic@blazorise.com>
2024-05-07 10:09:24 +0200

    Add C# implementation of Q_rsqrt

---
+++ Oats/Quake3.cs
@@ -0,0 +1,18 @@
+static void Main( string[] args )
+{
+    Console.WriteLine( $"{Q_rsqrt( 2.43f )}" );
+}
+
+static float Q_rsqrt( float number )
+{
+    float x2 = number * 0.5F;
+    float y = number;
+    int i = BitConverter.SingleToInt32Bits( y );
+
+    i = 0x5f3759df - ( i >> 1 );
+    y = BitConverter.Int32BitsToSingle( i );
+
+    y = y * ( 1.5F - ( x2 * y * y ) );
+
+    return y;
+}
