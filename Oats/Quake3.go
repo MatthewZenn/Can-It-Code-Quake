2fedb414d83f579d3dbfd10be23c5102fb7c1014
Bipin <87369440+bipinkrish@users.noreply.github.com>
2024-05-07 12:53:04 +0530

    Create Quake3.go

---
+++ Oats/Quake3.go
@@ -0,0 +1,19 @@
+import "unsafe"
+
+func Q_rsqrt(number float32) float32 {
+    threehalfs := float32(1.5)
+    x2 := number * 0.5
+    y := number
+
+    i := *(*uint32)(unsafe.Pointer(&y))
+    i = 0x5f3759df - (i >> 1)
+    y = *(*float32)(unsafe.Pointer(&i))
+    y = y * (threehalfs - (x2 * y * y)) // first iteration 
+    // y = y * (threehalfs - (x2 * y * y)) // increased precision
+
+    return y
+}
+
+func main() {
+    println(Q_rsqrt(123.4))
+}
