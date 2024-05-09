7a21c36470f17147c2ecf7a051469c519f2080d8
Harshit Shrivastav <harshitshrivastav@hotmail.com>
2024-05-09 11:45:57 +0530

    Create Quake3.php

---
+++ Oats/Quake3.php
@@ -0,0 +1,18 @@
+<?php
+function Q_rsqrt($number) {
+    $threehalfs = 1.5;
+    $x2 = $number * 0.5;
+    $y = $number;
+    $i = unpack('l', pack('f', $y))[1]; 
+    $i = 0x5f3759df - ($i >> 1); 
+    $y = unpack('f', pack('l', $i))[1];
+    $y = $y * ($threehalfs - ($x2 * $y * $y));
+
+    // Uncomment the following line for the second iteration
+    //$y = $y * ($threehalfs - ($x2 * $y * $y)); // Second iteration
+
+    return $y;
+}
+
+echo Q_rsqrt(2.43);
+?>
