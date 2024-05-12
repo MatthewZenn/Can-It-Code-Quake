51343804a3a89cbd522a2d788f9fb7e870ab230e
Harshit Shrivastav <harshitshrivastav@hotmail.com>
2024-05-12 17:51:26 +0530

    Update Quake4.rb

--- Oats/Quake4.rb
+++ Oats/Quake4.rb
@@ -2,13 +2,10 @@ def q_rsqrt(number)
     three_halfs = 1.5
     x2 = number * 0.5
     y = number
-    i = [y].pack('f').unpack('l')[0] # Get the bit representation of the float
-    i = 0x5f3759df - (i >> 1) # Perform bit-level operations
-    y = [i].pack('l').unpack('f')[0] # Convert the modified bit representation back to a float
-    y = y * (three_halfs - (x2 * y * y)) # First iteration
-
-    # Uncomment the following line for the second iteration
-    # y = y * (three_halfs - (x2 * y * y)) # Second iteration
+    i = [y].pack('f').unpack('l')[0] 
+    i = 0x5f3759df - (i >> 1) 
+    y = [i].pack('l').unpack('f')[0] 
+    y = y * (three_halfs - (x2 * y * y))
 
     return y
 end
